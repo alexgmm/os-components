b7552dcf78dfe32fa1b3997c2ea2574e60845b5d
alexgmm <alexandre.guedes.martins@gmail.com>
2020-03-17 08:01:23 -0500

    Incrementando classes base

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #define RANDOM (0)
 #define GREEDY_JOBS (1)
 #define GREEDY_MACHINES (2)
+#define DUMMY (0)
 
 using namespace std;
 
@@ -48,29 +49,62 @@ class Solution {
 		iota(jobs_succ.begin() + 1, jobs_succ.end(), 1);
 		random_shuffle(jobs_succ.begin() + 1, jobs_succ.end());
 
-		for(int i = 1; i<=jobs_succ.size(); i++)
-			jobs_pred[jobs_succ[i]] = i;
+		for(int i = 1; i<=jobs_succ.size(); i++) jobs_pred[jobs_succ[i]] = i;
 
 		iota(machines_succ.begin() + 1, machines_succ.end(), 1);
 		random_shuffle(machines_succ.begin() + 1, machines_succ.end());
 
-		for(int i = 1; i<=machines_succ.size(); i++)
-			machines_pred[machines_succ[i]] = i;
+		for(int i = 1; i<=machines_succ.size(); i++) machines_pred[machines_succ[i]] = i;
 	}
 
 	void initGreedyJobs(){}
 	void initGreedyMachines(){}
 
-	unsigned int calcMakespan(){
+	void calc_makespan(){
 		makespan = 0;
+		int temp_makespan = 0, operation, operation_succ, _operation, queue_top = 0;
 		vector<unsigned int> degrees_in(instance.n_ops,0);
+		vector<unsigned int> op_queue;
 
-		for(int i = 1; i <= (n_jobs > n_machines ? n_jobs: n_machines); i++){
-			if(i <= n_jobs && jobs_succ[i] > 0) degrees_in[i]++;
-			if(i <= n_machines && machines_succ[i] > 0) degrees_in[i]++;
+		for(int i = 1; i <= (instance.n_jobs > instance.n_machines ? instance.n_jobs: instance.n_machines); i++){
+			if(i <= instance.n_jobs && jobs_succ[i] > 0) degrees_in[i]++;
+			if(i <= instance.n_machines && machines_succ[i] > 0) degrees_in[i]++;
+			if(!degrees_in[i]) op_queue.push_back(i);
 		}
 
-		return makespan;
+		while(queue_top < op_queue.size()){
+			operation = op_queue(queue_top);
+
+			temp_makespan = partial_scheduling[operation] + instance.cost[operation];
+			if(temp_makespan > makespan){ 
+				makespan = temp_makespan;
+				_operation = operation;
+			}
+
+			operation_succ = jobs_succ[operation];
+
+			if (operation_succ != DUMMY){
+				degrees_in[operation_succ]--;
+				if(!degrees_in[operation_succ]) op_queue.push_back(operation_succ);
+				if(partial_schedule[operation_succ] < temp_makespan){
+					partial_schedule[operation_succ] = temp_makespan;
+					prev[operation_succ] = operation; //??????
+				}
+			}
+
+
+			operation_succ = machines_succ[operation];
+			if (operation_succ != DUMMY){
+				degrees_in[operation_succ]--;
+				if(!degrees_in[operation_succ]) op_queue.push_back(operation_succ);
+				if(partial_schedule[operation_succ] < temp_makespan){
+					partial_schedule[operation_succ] = temp_makespan;
+					prev[operation_succ] = operation; //??????
+				}
+			}
+
+			queue_top++; //Proxima operacao da fila
+		}
 	}
 public:
 	Solution () {}
@@ -80,10 +114,10 @@ public:
 		jobs_pred.resize(instance.n_jobs + 1);
 		machines_succ.resize(instance.n_machines + 1);
 		machines_pred.resize(instance.n_machines + 1);
-
+		//Cria solucao inicial
 		init(init_method);
-
-		makespan = calcMakespan();
+		
+		calc_makespan();
 	}
 
 	void init(int method){
